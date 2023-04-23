4db3bf3bb9cbd8dc72afa129e99ed24a6446c14e
FeJS8888 <fmxfmx2007@163.com>
2023-04-23 19:32:09 +0800

    Test

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,12 +1,13 @@
 #include<graphics.h>
 #include<vector>
+#include<string>
 #include<algorithm>
 #include<iostream>
 #include<queue>
 using namespace std;
 
 int keymsg;
-bool mousehit;
+bool mousehit = false;
 mouse_msg mouseinfo;
 
 //Classes
@@ -57,6 +58,7 @@ class Element {
 			this->scale = 100;
 			this->angle = 0;
 			this->order = 0;
+			for(int i = 0;i < 10;++ i) this->private_variables[i] = 0;
 		}
 		void call() {
 			this->reflush_mouse_statu();
@@ -161,16 +163,16 @@ class Element {
 		inline void push_function(auto function){
 			this->function_vector.push_back(function) ;
 		}
-		inline bool ismousein(int x,int y){
+		inline bool ismousein(){
+			int x,y;
+			mousepos(&x,&y);
 			int d_width = getwidth(this->image_vector[this->current_image]) / 2;
 			int d_height = getheight(this->image_vector[this->current_image]);
 			return (x >= this->pos.x - d_width && x <= this->pos.x + d_width && y >= this->pos.y - d_height && y <= this->pos.y + d_height);
 		}
 		inline bool ishit(){
 			if(!mousehit) return false;
-			int x,y;
-			mousepos(&x,&y);
-			return this->ismousein(x,y);
+			return this->ismousein();
 		}
 		inline void set_variable(unsigned int which,int value){
 			this->private_variables[which] = value;
@@ -203,28 +205,50 @@ void reflush() {
 	}
 	delay_ms(0);
 	keymsg = -1;
-	mousehit = false;
 	if(kbhit()) keymsg = getch();
 	if(mousemsg()){
-		mousehit = true;
 		mouseinfo = getmouse();
-	}
-	flushkey();
-}
-
-void fun(Element* e){
-//	e->move_right(1);
-	if(e->ishit()) e->set_variable(0,1);
-	else {
-		if(!e->get_variable(0)) return;
-		int x,y;
-		mousepos(&x,&y);
-		if(e->ismousein(x,y)){
-			e->set_variable(0,0);
-			e->hide();
+		if(mouseinfo.is_left()){
+			cout<<">>>"<<mousehit<<" ";
+			mousehit = (mousehit ? false : true);
+			cout<<mousehit<<"<<<"<<endl;
 		}
 	}
+	flushkey();
+	flushmouse();
 }
+
+//void fun(Element* e){
+////	e->move_right(1);
+//	if(e->ishit()) e->set_variable(0,1);
+//	else {
+//		if(!e->get_variable(0)) return;
+//		int x,y;
+//		mousepos(&x,&y);
+//		if(e->ismousein(x,y)){
+//			e->set_variable(0,0);
+//			e->hide();
+//		}
+//	}
+//}
+
+void test(Element* e){
+	if(e->get_variable(0) == 3) e->set_variable(0,0);
+	if(e->ismousein()){
+		cout<<"IN";
+		int statu = e->get_variable(0);
+		if(e->ishit()) {
+			cout<<"HIT";
+			if(statu == 1) e->set_variable(0,2);
+		}
+		else {
+			cout<<"NOT_HIT";
+			if(statu == 0) e->set_variable(0,1);
+			else if(statu == 2) e->set_variable(0,3);
+		}
+	}
+	else e->set_variable(0,0);
+} 
 void fun1(Element* e){
 	e->move_down(1);
 }
@@ -241,7 +265,7 @@ int main() {
 	Element e0 = *(new Element(e0_image,50,50));
 	reg_Element(&e0);
 	e0.show(); 
-	e0.push_function(fun);
+	e0.push_function(test);
 //	e0.push_function(fun1);
 //	e0.push_function(fun2); 
 	while(true){
@@ -250,8 +274,8 @@ int main() {
 //		if(keymsg == (int)('a' + ('s' - 'a'))) e0.move_down(5);
 //		if(keymsg == (int)('a' + ('d' - 'a'))) e0.move_right(5);
 
-
-		cout<<mousehit<<" "<<e0.ishit()<<endl;
+	cout<<e0.get_variable(0)<<" "<<e0.ismousein()<<endl; 
+//		cout<<mousehit<<" "<<e0.ishit()<<endl;
 		reflush();
 	}
 }
